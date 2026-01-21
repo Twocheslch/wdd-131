b950900bdf1af2ab782c4430a2dddf2d7e879869
Twocheslch <iracches@gmail.com>
2026-01-20 17:46:36 -0700

    bad code in w3 ponder

---
+++ week3/ponder/script.js
@@ -0,0 +1,18 @@
+@media screen and (min-width: 700px) {
+    const element = document.queryselector('')
+}
+          
+
+@media screen and (min-width: 480px) {
+  body {
+    .hide 
+        display: block;
+  }
+}    
+
+@media screen and (min-width: 1024px) {
+  body {
+    .menu-btn.classList.remove
+  }
+}    
+
