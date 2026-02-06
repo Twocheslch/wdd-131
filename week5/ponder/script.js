8198b23fe4e6252038cc9c021ff361795f82cb07
Twocheslch <iracches@gmail.com>
2026-02-05 23:23:59 -0700

    finished w5 ponder

--- week5/ponder/script.js
+++ week5/ponder/script.js
@@ -1,10 +1,7 @@
-$0.innerText
-$0.innerText.length
+const allRatingElems = document.querySelectorAll("article.movie p span");
 
-
-const allRatingElems = document.querySelectorAll('article.movie p span');
-
-allRatingElems.forEach((elem) => {
-    const rating = elem.innerText.length;
-    elem.setAttribute('aria-label', `${rating} out of 7 stars`)
+allRatingElems.forEach(function (elem) {
+  const rating = elem.innerText.length;
+  elem.setAttribute("aria-label", rating + " out of 5 stars");
+  elem.setAttribute("role", "img");
 });
